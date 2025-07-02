1e62c703bfd261224b1991de05c09c133bf9465a
Dev Erick <157327151+ErickMenezes321995@users.noreply.github.com>
2025-07-02 16:17:37 -0400

    alterações

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,4 +1,4 @@
-import {
+import { 
   Grid,
   GridItem,
   Drawer,
@@ -30,10 +30,6 @@ const navLinks = [
     name: "Sobre",
     path: "/sobre",
   },
-  // {
-  //   name: "Time",
-  //   path: "/team",
-  // },
   {
     name: "Notícias",
     path: "/noticias",
@@ -68,162 +64,156 @@ const Header = () => {
     alignItems: "center",
   };
 
+  const listenScrollEvent = () => {
+    if (window.scrollY > 70) {
+      setHeaderColor("#0064009D"); // verde com transparência
+    } else {
+      setHeaderColor("#006400"); // verde sólido
+    }
+  };
+
   useEffect(() => {
+    listenScrollEvent(); // chama logo ao montar para setar cor correta
     window.addEventListener("scroll", listenScrollEvent);
     return () => {
       window.removeEventListener("scroll", listenScrollEvent);
     };
-   
   }, []);
 
-  const listenScrollEvent = () => {
-    if (window.scrollY > 70) {
-      setHeaderColor("#0064009D");
-    } else {
-      setHeaderColor("#006400"); 
-    }
-  };
-  
+  return (
+    <>
+      <Flex
+        id="header"
+        align="center"
+        justify="space-between"
+        bg={location.pathname === "/" ? headerColor : "#006400"}
+        h={{ base: "5.75em", lg: "8.25em" }}
+        p={{
+          base: "0 1em",
+          sm: "0.75em 1em",
+          md: "0.75em 0.85em",
+          lg: "0 4.5em",
+          "2xl": "0 7.5em",
+        }}
+        zIndex="5"
+        top="0"
+        position="sticky"
+        transition="background-color 0.5s ease"
+      >
+        <Flex
+          as={NavLink}
+          to="/"
+          align="center"
+          height={{ base: "3em", md: "4em" }}
+        >
+          <Image
+            height={{ base: "2.75em", sm: "3em", md: "3.5em", lg: "4em" }}
+            src={CoritibaLogoNotWhite}
+          />
+        </Flex>
 
-return (
-           <>
-            <Flex
-              id="header"
-              align="center"
-              justify="space-between"
-              bg={location.pathname === "/" ? headerColor : "#006400"}
-              h={{ base: "5.75em", lg: "8.25em" }}
-              p={{
-                base: "0 1em",
-                sm: "0.75em 1em",
-                md: "0.75em 0.85em",
-                lg: "0 4.5em",
-                "2xl": "0 7.5em",
+        <Flex
+          align="center"
+          justify="space-between"
+          w={{ md: "95%", lg: "85%", "2xl": "60%" }}
+          minW="60%"
+          display={{ base: "none", lg: "flex" }}
+        >
+          {navLinks.map((link) => (
+            <Box
+              key={link.name}
+              as={NavLink}
+              to={link.path}
+              color="neutral.white"
+              borderBottom="3px solid transparent"
+              paddingBottom="0.5em"
+              __css={{
+                "&:hover": {
+                  borderColor: `${theme.colors.yellow[400]}`,
+                  color: `${theme.colors.yellow[400]}`,
+                },
               }}
-              zIndex="5"
-              top="0"
-              position="sticky"
-              transition="background-color 0.5s ease"
+              style={({ isActive }: { isActive: boolean }) => ({
+                ...defaultStyle,
+                ...(isActive ? activeStyle : null),
+              })}
             >
-              <Flex
-                as={NavLink}
-                to="/"
-                align="center"
-                height={{ base: "3em", md: "4em" }}
+              <Heading size="xs">{link.name}</Heading>
+            </Box>
+          ))}
+
+          <StoreButton />
+
+          <Flex flexDirection="row">
+            <SocialMedia iconColor="white" />
+          </Flex>
+        </Flex>
+
+        <IconButton
+          aria-label="Open Menu"
+          onClick={onOpen}
+          icon={<HiMenu size="2em" />}
+          size="lg"
+          variant="ghost"
+          colorScheme="blue"
+          color="blue.100"
+          display={{ base: "flex", lg: "none" }}
+        />
+      </Flex>
+
+      <Drawer isOpen={isOpen} placement="right" onClose={onClose}>
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerCloseButton color="yellow.400" />
+
+          <DrawerBody bg="#006400">
+            <Grid templateRows="repeat(8, 1fr)" gap="1.5em">
+              {navLinks.map((link) => (
+                <GridItem
+                  key={link.name}
+                  w="100%"
+                  display="flex"
+                  alignItems="center"
+                  justifyContent="center"
+                  fontWeight="700"
+                  color="neutral.white"
+                >
+                  <NavLink
+                    to={link.path}
+                    style={({ isActive }: { isActive: boolean }) => ({
+                      ...defaultStyle,
+                      ...(isActive ? activeStyle : null),
+                    })}
+                    onClick={onClose}
+                  >
+                    <Heading size="xs">{link.name}</Heading>
+                  </NavLink>
+                </GridItem>
+              ))}
+              <GridItem
+                w="100%"
+                display="flex"
+                alignItems="center"
+                justifyContent="center"
               >
-                <Image
-                  height={{ base: "2.75em", sm: "3em", md: "3.5em", lg: "4em" }}
-                  src={CoritibaLogoNotWhite}
-                />
-              </Flex>
-
-              <Flex
-                align="center"
-                justify="space-between"
-                w={{ md: "95%", lg: "85%", "2xl": "60%" }}
-                minW="60%"
-                display={{ base: "none", lg: "flex" }}
-              >
-                {navLinks.map((link) => (
-                  <Box
-                      key={link.name}
-                      as={NavLink}
-                      to={link.path}
-                      color="neutral.white"
-                      borderBottom="3px solid transparent"
-                      paddingBottom="0.5em"
-                      __css={{
-                        "&:hover": {
-                          borderColor: `${theme.colors.yellow[400]}`,
-                          color: `${theme.colors.yellow[400]}`,
-                        },
-                      }}
-                      style={({ isActive }: { isActive: boolean }) => ({
-                        ...defaultStyle,
-                        ...(isActive ? activeStyle : null),
-                      })}
-                    >
-                      <Heading size="xs">{link.name}</Heading>
-                    </Box>
-
-                ))}
-
                 <StoreButton />
-
-                <Flex flexDirection="row">
-                  <SocialMedia iconColor="white" />
-                </Flex>
-              </Flex>
-
-              <IconButton
-                aria-label="Open Menu"
-                onClick={onOpen}
-                icon={<HiMenu size="2em" />}
-                size="lg"
-                variant="ghost"
-                colorScheme="blue"
-                color="blue.100"
-                display={{ base: "flex", lg: "none" }}
-              />
-            </Flex>
-
-            <Drawer isOpen={isOpen} placement="right" onClose={onClose}>
-              <DrawerOverlay />
-              <DrawerContent>
-                <DrawerCloseButton color="yellow.400" />
-
-                <DrawerBody bg="#006400">
-                  <Grid templateRows="repeat(8, 1fr)" gap="1.5em">
-                    {navLinks.map((link) => (
-                      <GridItem
-                        key={link.name}
-                        w="100%"
-                        display="flex"
-                        alignItems="center"
-                        justifyContent="center"
-                        fontWeight="700"
-                        color="neutral.white"
-                      >
-                        <NavLink
-                            to={link.path}
-                          style={({ isActive }: { isActive: boolean }) => ({
-                            ...defaultStyle,
-                            ...(isActive ? activeStyle : null),
-                          })}
-
-
-                            onClick={onClose}
-                          >
-                            <Heading size="xs">{link.name}</Heading>
-                          </NavLink>
-
-                      </GridItem>
-                    ))}
-                    <GridItem
-                      w="100%"
-                      display="flex"
-                      alignItems="center"
-                      justifyContent="center"
-                    >
-                      <StoreButton />
-                    </GridItem>
-                    <GridItem
-                      w="100%"
-                      h="10"
-                      display="flex"
-                      alignItems="center"
-                      justifyContent="center"
-                      pt="3em"
-                    >
-                      <SocialMedia iconColor="white" />
-                    </GridItem>
-                  </Grid>
-                </DrawerBody>
-              </DrawerContent>
-            </Drawer>
-          </>
-        );
-
-      };
+              </GridItem>
+              <GridItem
+                w="100%"
+                h="10"
+                display="flex"
+                alignItems="center"
+                justifyContent="center"
+                pt="3em"
+              >
+                <SocialMedia iconColor="white" />
+              </GridItem>
+            </Grid>
+          </DrawerBody>
+        </DrawerContent>
+      </Drawer>
+    </>
+  );
+};
 
 export default Header;
